4efc002e60a4d31586179801e61ed60a73d13ef3
Junnyjun <chbe5082@naver.com>
2024-08-16 17:02:43 +0900

    Init

---
+++ src/main/kotlin/git/io/axon/AxonApplication.kt
@@ -0,0 +1,11 @@
+package git.io.axon
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class AxonApplication
+
+fun main(args: Array<String>) {
+    runApplication<AxonApplication>(*args)
+}
